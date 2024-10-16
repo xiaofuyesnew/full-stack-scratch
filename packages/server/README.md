bbb4aff38411824784efd01f26b1ccadf9db8909
huanghs <xiaofuyesnew@qq.com>
2024-10-16 13:00:54 +0800

    feat: day 3 run front and back together

---
+++ packages/server/README.md
@@ -0,0 +1,5 @@
+## @fss/server
+
+## issues
+
+[vite-plugin-node 升级到 4.0.0 后无法打包成 ESM 的问题](https://github.com/axe-me/vite-plugin-node/issues/114)
---
+++ packages/server/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import { VitePluginNode } from 'vite-plugin-node'
+
+export default defineConfig({
+  plugins: [
+    ...VitePluginNode({
+      adapter: 'koa',
+      appPath: './src/main.js',
+      exportName: 'viteNodeApp',
+      initAppOnBoot: false,
+      tsCompiler: 'esbuild',
+    }),
+  ],
+  server: {
+    port: 3000,
+    hmr: true,
+  },
+})
